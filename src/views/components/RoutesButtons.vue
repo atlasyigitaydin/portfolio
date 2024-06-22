4c07e360f8d394c6928cae3e4f925cb1a8f270e1
Atlas <atlasyigitaydin@gmail.com>
2024-06-22 21:23:09 +0300

    theme

--- src/views/components/RoutesButtons.vue
+++ src/views/components/RoutesButtons.vue
@@ -10,7 +10,7 @@ defineProps<{
     :label="routes.label"
     :icon="`${routes.icon} text-lg`"
     style="width: 80%;"
-    class="shadow-8 h-3rem border-round justify-content-between px-3 flex align-items-center"
+    class="shadow-8 h-3rem border-round justify-content-between px-3 flex align-items-center text-lg"
     @click="routes.route"
   />
 </template>
