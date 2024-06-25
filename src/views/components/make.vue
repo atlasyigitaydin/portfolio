f9407b75ad1243b02ace5cd3687f580c2612f481
Atlas <atlasyigitaydin@gmail.com>
2024-06-25 18:19:59 +0300

    in make add back to hame page

--- src/views/components/make.vue
+++ src/views/components/make.vue
@@ -2,8 +2,11 @@
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-content-center align-items-center gap-2">
-    <div class="pi pi-cog pi-spin" />
-    <label> {{ "Under Construction" }} </label>
+  <div class="w-full h-full flex flex-column justify-content-center align-items-center gap-2">
+    <div class="w-full h-full flex justify-content-center align-items-center gap-2">
+      <div class="pi pi-cog pi-spin" />
+      <label> {{ "Under Construction" }} </label>
+    </div>
+    <BackToHomePage />
   </div>
 </template>
