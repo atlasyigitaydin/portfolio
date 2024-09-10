672eae0ffffc696e4f7cfec7f9cc6fc560b28dc4
Atlas <atlasyigitaydin@gmail.com>
2024-09-11 00:29:16 +0300

    new file

---
+++ .nuxt/prerender/chunks/build/index-Ch7Xbplo.mjs
@@ -0,0 +1,56 @@
+import { v as vueExports, s as ssrRenderAttrs_1 } from './server.mjs';
+import 'node:http';
+import 'node:https';
+import 'node:zlib';
+import 'node:stream';
+import 'node:buffer';
+import 'node:util';
+import 'node:url';
+import 'node:net';
+import 'node:fs';
+import 'node:path';
+import '../_/renderer.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/vue-bundle-renderer@2.1.0/node_modules/vue-bundle-renderer/dist/runtime.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/h3@1.12.0/node_modules/h3/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/devalue@5.0.0/node_modules/devalue/index.js';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/ufo@1.5.4/node_modules/ufo/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/vue@3.5.4_typescript@5.6.2/node_modules/vue/server-renderer/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/@unhead+ssr@1.11.2/node_modules/@unhead/ssr/dist/index.mjs';
+import '../runtime.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/ofetch@1.3.4/node_modules/ofetch/dist/node.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/destr@2.0.3/node_modules/destr/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unenv@1.10.0/node_modules/unenv/runtime/fetch/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/hookable@5.5.3/node_modules/hookable/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/klona@2.0.6/node_modules/klona/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/scule@1.3.0/node_modules/scule/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/defu@6.1.4/node_modules/defu/dist/defu.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/ohash@1.1.3/node_modules/ohash/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unstorage@1.12.0_ioredis@5.4.1/node_modules/unstorage/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unstorage@1.12.0_ioredis@5.4.1/node_modules/unstorage/drivers/fs.mjs';
+import 'file:///C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/core/runtime/nitro/cache-driver.js';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unstorage@1.12.0_ioredis@5.4.1/node_modules/unstorage/drivers/fs-lite.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/radix3@1.1.2/node_modules/radix3/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/pathe@1.1.2/node_modules/pathe/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unhead@1.11.2/node_modules/unhead/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/vue@3.5.4_typescript@5.6.2/node_modules/vue/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/@unhead+shared@1.11.2/node_modules/@unhead/shared/dist/index.mjs';
+import 'file://C:/Users/evane/Desktop/Portfolio_Website/node_modules/.pnpm/unctx@2.3.1/node_modules/unctx/dist/index.mjs';
+
+const _sfc_main = /* @__PURE__ */ vueExports.defineComponent({
+  __name: "index",
+  __ssrInlineRender: true,
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<div${ssrRenderAttrs_1(vueExports.mergeProps({ class: "h-[800px] text-center w-full flex justify-center items-center overflow-auto" }, _attrs))}><div class="w-5/12 text-center"> works </div></div>`);
+    };
+  }
+});
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = vueExports.useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/works/index.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=index-Ch7Xbplo.mjs.map
---
+++ .nuxt/prerender/chunks/build/styles.mjs
@@ -0,0 +1,15 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.CIGQ2jO2.mjs').then(interopDefault),
+  "app.vue": () => import('./app-styles.DeuAA_PM.mjs').then(interopDefault),
+  "app.vue?vue&type=style&index=0&lang.css": () => import('./app-styles.MAL_qDW9.mjs').then(interopDefault),
+  "node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.bvJZJVVC.mjs').then(interopDefault),
+  "node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.CB2QsNYv.mjs').then(interopDefault),
+  "node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=0154b231&lang.css": () => import('./error-500-styles.DRr17-G2.mjs').then(interopDefault),
+  "node_modules/.pnpm/nuxt@3.13.1_@types+node@22.5.4_eslint@8.57.0_rollup@3.29.4_typescript@5.6.2_vite@5.4.3/node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=81dffb87&lang.css": () => import('./error-404-styles.BjQCL4ej.mjs').then(interopDefault),
+  "layouts/default.vue": () => import('./default-styles.-01cOK0v.mjs').then(interopDefault),
+  "layouts/default.vue?vue&type=style&index=0&scoped=fbd21167&lang.css": () => import('./default-styles.B4Vtmbwv.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
