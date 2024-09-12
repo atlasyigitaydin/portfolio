d36f8379b98b9b3e51cb53d615ada8a7358c48b1
Atlas <atlasyigitaydin@gmail.com>
2024-09-12 23:11:32 +0300

    home

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,16 +1,56 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+
+// Page metadata
 definePageMeta({
   name: 'contact',
   path: '/contact',
   validate: route => !!route,
   key: route => route.fullPath
 })
+
+const email = ref<string>('')
+const message = ref<string>('')
+
+const sendEmail = () => {
+  if (!email.value || !message.value) {
+    return
+  }
+
+  email.value = ''
+  message.value = ''
+}
 </script>
 
 <template>
-  <div class="h-[800px] text-center w-full flex justify-center items-center overflow-auto">
-    <div class="w-5/12 text-center">
-      contact me
+  <div class="w-full h-full flex flex-col justify-end items-center">
+    <div class="flex flex-col lg:h-4/6 lg:w-5/12 w-full h-full gap-6 p-6">
+      <input
+        v-model="email"
+        class="transition-all outline-0 duration-500 ease-in-out hover:border-zinc-600 border-b border-zinc-900 bg-transparent text-xl focus:border-none py-3 px-6"
+        placeholder="example@example.com"
+        type="email"
+      >
+
+      <textarea
+        v-model="message"
+        class="transition-all outline-0 duration-500 ease-in-out hover:border-zinc-600 border border-zinc-900 h-full bg-transparent text-xl focus:border-none py-3 px-6"
+        placeholder="your message"
+      />
+
+      <button class="headerButtons" @click="sendEmail">
+        send (not working)
+      </button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.headerButtons {
+    @apply cursor-pointer shadow-lg py-2 lg:text-lg text-sm border border-zinc-900 lg:opacity-80 font-extralight transition-all duration-500 ease-in-out z-10;
+}
+
+.headerButtons:hover {
+    @apply font-light opacity-100 lg:text-xl text-base lg:mx-8 mx-6 border-zinc-600;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,50 +2,28 @@
 definePageMeta({
   name: 'home',
   path: '/'
+  // layout: false
 })
 </script>
 
 <template>
   <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center z-0">
     <div class="w-full flex flex-col h-full items-center justify-center">
-      <!-- Add animation classes for each label -->
-      <label class="lg:text-[180px] md:text-[130px] text-[50px] font-thin flex w-full justify-start animate-fade-slide-in-left">FRONTEND</label>
-      <label class="lg:text-[160px] md:text-[120px] text-[40px] font-thin flex w-full justify-end animate-fade-slide-in-right">DEVELOPER</label>
+      <div class="flex w-full">
+        <label class="lg:text-[180px] md:text-[130px] text-[50px] font-thin flex w-full justify-start">FRONTEND</label>
+        <!-- <div class="flex flex-col justify-center gap-3 py-12 px-4 items-around w-full">
+          <label> ATLAS </label>
+          <label> YİĞİT </label>
+          <label> AYDIN </label>
+        </div> -->
+      </div>
+      <label class="lg:text-[160px] md:text-[120px] text-[40px] font-thin flex w-full justify-end">DEVELOPER</label>
     </div>
     <div class="w-full ">
-      <AbilitySlider />
+      <!-- <AbilitySlider /> -->
     </div>
   </div>
 </template>
 
 <style scoped>
-@keyframes fadeSlideInLeft {
-  0% {
-    opacity: 0;
-    transform: translateX(-100px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-
-@keyframes fadeSlideInRight {
-  0% {
-    opacity: 0;
-    transform: translateX(100px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-
-.animate-fade-slide-in-left {
-  animation: fadeSlideInLeft 1.5s ease-in-out forwards;
-}
-
-.animate-fade-slide-in-right {
-  animation: fadeSlideInRight 1.5s ease-in-out forwards;
-}
 </style>
