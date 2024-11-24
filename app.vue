9993338a0cb8abead0420933236fc7ede7af7578
Atlas <atlasyigitaydin@gmail.com>
2024-11-24 17:13:57 +0300

    firebase

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { initializeApp } from 'firebase/app'
+
+const config = useRuntimeConfig()
+
+const firebaseConfig = {
+  apiKey: config.public.firebaseApiKey as string,
+  authDomain: config.public.firebaseAuthDomain as string,
+  projectId: config.public.firebaseProjectId as string,
+  storageBucket: config.public.firebaseStorageBucket as string,
+  messagingSenderId: config.public.firebaseMessagingSenderId as string,
+  appId: config.public.firebaseAppId as string,
+}
+
+initializeApp(firebaseConfig)
+</script>
+
 <template>
   <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center font-extralight text-2xl bg-zinc-900 text-white">
     FoxJs
