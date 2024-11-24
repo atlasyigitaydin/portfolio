55c92c79e33b2d743726c0271a66529b18648e55
Atlas <atlasyigitaydin@gmail.com>
2024-11-24 18:23:57 +0300

    cancel firebase

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
-import { initializeApp } from 'firebase/app'
 
-const config = useRuntimeConfig()
-
-const firebaseConfig = {
-  apiKey: config.public.firebaseApiKey as string,
-  authDomain: config.public.firebaseAuthDomain as string,
-  projectId: config.public.firebaseProjectId as string,
-  storageBucket: config.public.firebaseStorageBucket as string,
-  messagingSenderId: config.public.firebaseMessagingSenderId as string,
-  appId: config.public.firebaseAppId as string,
-}
-
-initializeApp(firebaseConfig)
 </script>
 
 <template>
